9b7e21b93e0d52b478ae052e85f9c24c0469e010
martin-fll <30398335+martin-fll@users.noreply.github.com>
2017-10-09 19:43:49 +1300

    jenkins

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,6 @@
-rsync -o StrictHostKeyChecking=no docker-* deployer@$1:
+set -e
+
+rsync -e "ssh -o StrictHostKeyChecking=no" docker-* deployer@$1:
 
 ssh -o StrictHostKeyChecking=no deployer@$1 "echo \"CATCHHUB_VERSION=$2\" >.env"
 ssh -o StrictHostKeyChecking=no deployer@$1 "./docker-update-and-restart.sh"
